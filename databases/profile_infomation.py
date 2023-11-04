fc3d9868a1a571f298822cfccbf4b1291fb148a4
Danae Troupe <danaetroupe@gmail.com>
2023-11-04 15:58:14 -0500

    set up profile_information

--- databases/profile_infomation.py
+++ databases/profile_infomation.py
@@ -1,8 +1,11 @@
 import os
 from pyairtable import Api
 
-db = Api(os.environ['patpQyRt1AX0n8eGS.d9a9dcf4bb1239000fcbbf2dd92bf0dba8e68d9dc955c7f221f0b71665592564'])
-airports = db.table('Airports')
+API_TOKEN = 'patpQyRt1AX0n8eGS.b5eacd0c39dd0476476a7361f4d7a15a223f054cc1e6443a9580d26231b513d5'
+db = Api(API_TOKEN)
+
+airports = db.table('app66AlHq30vppBQp', 'Airports')
+pairings = db.table('app66AlHq30vppBQp', 'Pairings')
 
 for records in airports.iterate():
     print(records)
