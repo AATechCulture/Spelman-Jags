769011010afda8b0c53f921536e79a3ee409628c
Danae Troupe <danaetroupe@gmail.com>
2023-11-04 12:31:53 -0500

    how to use poetry

---
+++ poetry.py
@@ -0,0 +1,4 @@
+# https://python-poetry.org/docs/basic-usage/#using-poetry-run
+# install poetry and then enter "poetry shell" to enter virtual enviornment 
+# (you may have to navigate into hackathon-practice)
+# use "poetry install" to get all dependencies
