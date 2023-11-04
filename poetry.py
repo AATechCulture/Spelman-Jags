a0696daa40ebf94c5985cb079aa6ca97d5ad30e8
Danae Troupe <danaetroupe@gmail.com>
2023-11-04 12:34:31 -0500

    updated poetry instructions

--- poetry.py
+++ poetry.py
@@ -1,4 +1,3 @@
 # https://python-poetry.org/docs/basic-usage/#using-poetry-run
 # install poetry and then enter "poetry shell" to enter virtual enviornment 
-# (you may have to navigate into hackathon-practice)
 # use "poetry install" to get all dependencies
