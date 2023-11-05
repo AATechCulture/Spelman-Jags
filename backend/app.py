2c07f329d8c45a070f75a8c053c5759cc7872250
Danae Troupe <danaetroupe@gmail.com>
2023-11-04 21:37:10 -0500

    dependency updates

--- backend/app.py
+++ backend/app.py
@@ -1,2 +1,44 @@
-import profile_infomation as profile
+from flask import flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_marshmallow import Marshmallow
+from flask_cors import CORS
 
+db = SQLAlchemy()
+migrate = Migrate()
+ma = Marshmallow()
+cors = CORS()
+
+def create_app():
+    """Application-factory pattern"""
+    app = flask(__name__)
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    # Initialize extensions
+    # To use the application instances above, instantiate with an application:
+    db.init_app(app)
+    migrate.init_app(app, db)
+    ma.init_app(app)
+    cors.init_app(app)
+
+    return app
+
+def deploy():
+    """Run deployment tasks."""
+    from app import create_app,db
+    from flask_migrate import upgrade,migrate,init,stamp
+    from models import Articles
+
+    app = create_app()
+    app.app_context().push()
+
+    # create database and tables
+    db.create_all()
+
+    # migrate database to latest revision
+    stamp()
+    migrate()
+    upgrade()
+
+deploy()
